3194e32342466232f77401449ace3d1e1c576e42
Josh <leong.jo@gmail.com>
2017-12-05 20:04:04 -0700

    initial commit

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+export const facebook = {
+    clientID: '149300379172316',
+    clientSecret: '908ca765d8a4692a3c994f00854ae07c',
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
+    profileFields: ['id', 'name', 'displayName', 'picture', 'email'],
+  };
+  
+  export const google = {
+    clientID: '1067879356920-ef6msnpimrptjoimgi5scdqoiobaq3kh.apps.googleusercontent.com',
+    clientSecret: 'ukI7NacjGhEngDVtGEEW_Hqg',
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+  };
